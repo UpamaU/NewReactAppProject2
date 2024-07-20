d9135fc59a21f77915d3da5a94ea3e130f40309e
UpamaU <uutpa088@uottawa.ca>
2024-07-20 18:02:32 -0400

    should run now

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import './About.css';
 
 const About = () => {
-  const { t, i18n } = useTranslation();
+  const {i18n } = useTranslation();
 
   return (
     <div className="about">
